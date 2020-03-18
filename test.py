157e5bba105d82c5efc81f5291ff6837ced3e930
Peng <17098533pa11001@student.must.edu.mo>
2020-03-19 00:25:23 +0800

    Good job little old bro

---
+++ test.py
@@ -0,0 +1,12 @@
+'''
+@Author: your name
+@Date: 2020-03-19 00:18:01
+@LastEditTime: 2020-03-19 00:19:20
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /test/test.py
+'''
+
+def test():
+    pass
+
